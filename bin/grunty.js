4f21439552967eb8185565cfc19d1c6e11a0ba2d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-06-02 22:48:59 -0400

    trying to load and run concat task without gruntfile

--- bin/grunty.js
+++ bin/grunty.js
@@ -3,4 +3,32 @@
 console.log('grunty in', process.cwd());
 
 var grunt = require('grunt');
-grunt.cli();
+
+// console.log(grunt.task.registerTask.toString());
+
+var _exists = grunt.file.exists;
+grunt.file.exists = function mockExists(filename) {
+  console.log('mock exists', filename);
+  if (filename === 'Gruntfile.js') {
+    return true;
+  } else {
+    return _exists(filename)
+  }
+};
+
+grunt.task.loadNpmTasks('grunt-contrib-concat');
+// console.log('grunt.tasks', grunt.tasks);
+
+function done() {
+  console.log('grunt is done');
+}
+
+var options = {
+  version: true,
+  verbose: true
+};
+
+grunt.tasks(['concat'], options, done);
+
+// console.log(grunt.file);
+// grunt.cli();
